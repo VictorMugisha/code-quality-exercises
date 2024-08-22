6a4fb015ebcaf583d5c0fbd76d742543efabd961
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-22 09:00:47 +0200

    Refactored maxNumber function

---
+++ code-improvements/maxNumber/maxNumberNew.js
@@ -0,0 +1,6 @@
+// Make more descriptive function name and correct arguments in array
+function getMaximumNumber(...numbers) {
+    // Make use of built-in functions
+    const maximumNumber = Math.max(...numbers);
+    return maximumNumber;
+}
